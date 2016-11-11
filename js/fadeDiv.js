10f37ae48b8cd3bc1ac3cd6cb1e4e657a4e2a36b
nateschulte <nateschulte12@gmail.com>
2016-11-11 15:27:30 -0500

    add percolate

--- js/fadeDiv.js
+++ js/fadeDiv.js
@@ -5,8 +5,14 @@ $(document).ready(function(){
     $("body").addClass("remove-overflow");
  });
 
+ $(".percolate-row").click(function() {
+    $(".percolate-expanded").fadeIn("fast");
+    $("body").addClass("remove-overflow");
+ });
+
  $(".close").click(function() {
     $(".abacus-expanded").fadeOut("fast");
+    $(".percolate-expanded").fadeOut("fast");
     $("body").removeClass("remove-overflow");
  });
 
